c48fc0a2513b2e7c7bf048e05b9b4d99024723b2
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-08-19 16:12:56 +0500

    removed app.css file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 // Css files
-import "./css/app.css";
 import "./css/global.css";
 
 createRoot(document.getElementById("root")!).render(
